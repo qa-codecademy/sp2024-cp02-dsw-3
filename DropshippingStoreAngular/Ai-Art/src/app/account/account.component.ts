c2c4a3e6628fe6a162db5c2d3f77314d6a421f9c
Tamara Najdovska <139224391+tamaranaj@users.noreply.github.com>
2024-11-20 11:02:13 +0100

    Development (#44)
    * Feature/am10/home page (#28)
    
    * added home page
    
    * update
    
    * footer update
    
    * adding components (#29)
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * AboutPage (#30)
    
    * create contact page
    
    * New Update
    
    * loginRegister components
    
    * Bootstrap update
    
    * Feature/t18/generate art (#34)
    
    * create generate your art
    
    * add features
    
    * changes
    
    * changes
    
    * changes
    
    * add features
    
    * update style
    
    * update favorite and card icon
    
    * add guard on generate component
    
    * update form
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * Feature/cart (#35)
    
    * update cart
    
    * update cart
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * package-lock
    
    * account
    
    * adding css
    
    * visibility icon added
    
    * css added
    
    * Feature/#9/categories (#36)
    
    * updates
    
    * changes in store
    
    * conflicts resolving
    
    * update store
    
    * update generate component
    
    * update cart
    
    * fixing bugs in cart
    
    * update
    
    * updates
    
    * updates
    
    * updates
    
    * update features
    
    * style changes
    
    * updates
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * update
    
    * sold images added
    
    * Feature/recently viewed (#37)
    
    * update
    
    * update ux personalization
    
    * fix bugs and add new feature
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * fix style in home page (#38)
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * updates (#39)
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * updates (#40)
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * Fix/register (#41)
    
    * fix bugs
    
    * update
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * updates (#42)
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    * Documentation (#43)
    
    * add read me file
    
    * add images
    
    ---------
    
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    
    ---------
    
    Co-authored-by: Ana Mihajlovska <153089355+anamihajlovska@users.noreply.github.com>
    Co-authored-by: tamaranaj <najdovska18@gmail.com>
    Co-authored-by: Alek986 <149313522+Alek986@users.noreply.github.com>
    Co-authored-by: Peco <pecogramatikov@gmail.com>
    Co-authored-by: kirilfoxxxx <116038964+kirilfoxxxx@users.noreply.github.com>
    Co-authored-by: Ana Mihajlovska <anamihajlovskaa123@gmail.com>

--- DropshippingStoreAngular/Ai-Art/src/app/account/account.component.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/account/account.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AppStore } from '../store/app.store';
+
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [RouterModule ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
-
+  appStore = inject(AppStore)
+  constructor(private readonly authService: AuthService, private router: Router){
+  }
+  
+  handleLogout(){
+    this.appStore.setUser(undefined)
+    this.appStore.resetCart()
+    this.appStore.resetRecentOpen()
+    this.appStore.resetFavorites()
+    this.authService.logout()
+    this.router.navigate(['/'])
+    
+  }
 }
